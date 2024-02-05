cf1147c18fd0e38bbfde61b521bdd06ad4c93ba4
CoderKinley <kinleypenjor13@gmail.com>
2024-02-05 17:36:17 +0530

    new files created in the folder that i have made previously and accordingly added the logic

---
+++ MIV_System/decompressionValve.py
@@ -0,0 +1,84 @@
+from MIV_System.components.Pump import HydraulicPump
+from MIV_System.components.DirectionControlValve import DirectionControlValve
+from MIV_System.components.DoubleActingCylinder import HydarulicActuator
+
+class DecompressionValve:
+    def  __init__(
+        self, 
+        event_time,
+        stroke_position,
+        signal_flag_dac_bp_ext,
+        signal_flag_dac_bp_ret, 
+        last_stroke_position,
+        time_counter,
+        left_solenoid_sig,
+        right_solenoid_sig
+    ):
+        self.event_time = event_time
+        self.stroke_position =  stroke_position
+        self.signal_flag_dac_bp_ext = signal_flag_dac_bp_ext
+        self.signal_flag_dac_bp_ret = signal_flag_dac_bp_ret
+        self.last_stroke_position = last_stroke_position
+        self.time_counter = time_counter
+        self.left_solenoid_sig = left_solenoid_sig
+        self.right_solenoid_sig = right_solenoid_sig
+        
+    def run(self):
+        #test for hydraulic double acting actuator
+        bore_diameter = 0.094 # m
+        rod_diameter = 0.016 # mcls
+        stroke_length = 0.6 # m maximum length the pistion rod extends
+        initial_position = 0 # m initial position of the piston
+        simulation_time = 0.25 # s
+        discharge = 0.0032 # m^3/s
+        packingFriction = 0 # N
+        timer = self.time_counter
+        motor_speed = 3600 
+        oil_type = "skydrol_1"
+        operating_pressure = 7845320 # in pascals
+
+        efficiency = 0.85
+        mass =  50000 # in KG
+        pump_ON = True
+
+        # Calling the pump function
+        pump = HydraulicPump(            
+            oil_type, 
+            motor_speed, 
+            operating_pressure, 
+            mass, efficiency
+            )
+
+        density, viscosity, bulk_modulus = pump.getOil()
+        flow_rate, pump_pressure = pump.simulate(pump_ON)
+        
+        # calling the direction control valve function
+        dcv = DirectionControlValve(
+            density, 
+            viscosity, 
+            pump_pressure, 
+            flow_rate
+        )
+
+        port = dcv.simulate(self.left_solenoid_sig, self.right_solenoid_sig)
+        
+        #  calling the hydarulic actuator signal
+        hs = HydarulicActuator(
+            bore_diameter,
+            rod_diameter,
+            stroke_length,
+            initial_position,
+            simulation_time,
+            operating_pressure,
+            density,
+            discharge,
+            packingFriction,
+            timer, 
+            port,
+            self.event_time, # from global variable at top
+            self.stroke_position, # determines the current positon
+            self.signal_flag_dac_bp_ext,
+            self.signal_flag_dac_bp_ret, 
+            self.last_stroke_position
+        )
+        return hs.simulate
--- system.py
+++ system.py
@@ -1,13 +1,19 @@
-from MIV_System.components.Pump import HydraulicPump
-from MIV_System.components.DirectionControlValve import DirectionControlValve
-from MIV_System.components.DoubleActingCylinder import HydarulicActuator
 import keyboard
 import pyrebase
 import time
 import paho.mqtt.publish as publish
 import paho.mqtt.subscribe as subscribe
 
+from MIV_System.components.Pump import HydraulicPump
+from MIV_System.components.DirectionControlValve import DirectionControlValve
+from MIV_System.components.DoubleActingCylinder import HydarulicActuator
+from MIV_System.bypassValve import BypassValve
+from MIV_System.decompressionValve import DecompressionValve
+from MIV_System.serviceSealValve import ServiceSealValve
+from MIV_System.MIVValve import MIVValve
+
 keypress_val = 0
+
 class MainSystem:
     def __init__(self):
         self.time_counter = 0
@@ -122,8 +128,11 @@ class MainSystem:
             print("Time value--> ", self.event_time)
             time.sleep(counter_val)
                         
-            # connecting the bypass valve to the system
+            # calling the different valves functions
             displacement, q, f_extension, v_extension, power_input, power_output = self.bypass_valves()
+            self.miv_valve()
+            self.decompression_valve()
+            self.service_seal_valve()
             
             dataMqtt = ",".join(map(str,[
                 self.bypass_valves()
@@ -133,68 +142,34 @@ class MainSystem:
             
             # for setting the current postion of the piston stroke
             self.stroke_position = displacement 
+
+            # calling the firebase connection function to upload the data to the firebase
             self.connect_firebase(displacement, v_extension, q, f_extension, power_input, power_output)
 
-    # just the bypass logic
-    def bypass_valves(self):
-        #test for hydraulic double acting actuator
-        bore_diameter = 0.094 # m
-        rod_diameter = 0.016 # mcls
-        stroke_length = 0.6 # m maximum length the pistion rod extends
-        initial_position = 0 # m initial position of the piston
-        simulation_time = 0.25 # s
-        discharge = 0.0032 # m^3/s
-        packingFriction = 0 # N
-        timer = self.time_counter
-        motor_speed = 3600 
-        oil_type = "skydrol_1"
-        operating_pressure = 7845320 # in pascals
-
-        efficiency = 0.85
-        mass =  50000 # in KG
-        pump_ON = True
-
-        # Calling the pump function
-        pump = HydraulicPump(            
-            oil_type, 
-            motor_speed, 
-            operating_pressure, 
-            mass, efficiency
-            )
-
-        density, viscosity, bulk_modulus = pump.getOil()
-        flow_rate, pump_pressure = pump.simulate(pump_ON)
-        
-        # calling the direction control valve function
-        dcv = DirectionControlValve(
-            density, 
-            viscosity, 
-            pump_pressure, 
-            flow_rate
-        )
-
-        port = dcv.simulate(self.left_solenoid_sig, self.right_solenoid_sig)
-        
-        #  calling the hydarulic actuator signal
-        hs = HydarulicActuator(
-            bore_diameter,
-            rod_diameter,
-            stroke_length,
-            initial_position,
-            simulation_time,
-            operating_pressure,
-            density,
-            discharge,
-            packingFriction,
-            timer, 
-            port,
+    def initialize_valves(self, ValveClass):
+        return ValveClass(
             self.event_time, # from global variable at top
             self.stroke_position, # determines the current positon
             self.signal_flag_dac_bp_ext,
             self.signal_flag_dac_bp_ret, 
-            self.last_stroke_position
-        )
-        return hs.simulate
+            self.last_stroke_position,
+            self.time_counter,
+            self.left_solenoid_sig,
+            self.right_solenoid_sig
+        ).run()
+
+    # just the bypass logic
+    def bypass_valves(self):
+        return self.initialize_valves(BypassValve)
+    
+    def decompression_valve(self):
+        return self.initialize_valves(DecompressionValve)
+    
+    def service_seal_valve(self):
+        return self.initialize_valves(ServiceSealValve)
+    
+    def miv_valve(self):    
+        return self.initialize_valves(MIVValve)
     
 def main():
     main_system = MainSystem()
